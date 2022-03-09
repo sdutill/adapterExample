e536c13dbc98832e415ba2833b952a9bd5e03ae5
shawndutill <49755778+sdutill@users.noreply.github.com>
2022-03-09 14:36:15 -0500

    Initial Commit

--- app/src/main/java/edu/temple/adapterexample/MainActivity.kt
+++ app/src/main/java/edu/temple/adapterexample/MainActivity.kt
@@ -1,9 +1,10 @@
-package edu.temple.adapterexample
+package edu.temple.adapterviewapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import edu.temple.adapterexample.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,30 +14,49 @@ class MainActivity : AppCompatActivity() {
         val spinner = findViewById<Spinner>(R.id.spinner)
         val listView = findViewById<ListView>(R.id.listView)
 
-        // val colors = arrayOf("Red","Green","Blue","Yellow","Gold")
+        val colors = arrayOf("Red", "Green", "Yellow", "Blue", "Gray")
+/*
+        val adapter = ArrayAdapter(this
+            , android.R.layout.simple_spinner_dropdown_item
+            , colors)
 
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, colors)
-        
+ */
+
+        val adapter = ColorAdapter(this, colors)
+
         spinner.adapter = adapter
         listView.adapter = adapter
 
-        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                // toast is a temporary message that displays and disappears
 
-                val toastMessage = p0!!.getItemAtPosition(p2) as String
+        val eventListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+
+                val messageString = p0!!.getItemAtPosition(p2).toString()
 
                 Toast
-                    .makeText(this@MainActivity, toastMessage, Toast.LENGTH_SHORT)
+                    .makeText(this@MainActivity, messageString, Toast.LENGTH_SHORT)
                     .show()
             }
 
             override fun onNothingSelected(p0: AdapterView<*>?) {
             }
+
+        }
+
+        val onItemClickListener = object: AdapterView.OnItemClickListener {
+            override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                val messageString = p0!!.getItemAtPosition(p2).toString()
+
+                Toast
+                    .makeText(this@MainActivity, messageString, Toast.LENGTH_SHORT)
+                    .show()
+            }
+
         }
 
         spinner.onItemSelectedListener = eventListener
 
-        listView.onItemClickListener = AdapterView.OnItemClickListener( )
+        listView.onItemClickListener = onItemClickListener
+
     }
 }
---
+++ app/src/main/java/edu/temple/adapterexample/NumberAdapter.kt
@@ -0,0 +1,26 @@
+package edu.temple.adapterexample
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.content.Context
+
+class NumberAdapter(_context: Context, _n:Int) : BaseAdapter{
+    val context = _context
+    val n = _n
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItem(p0: Int): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemId(p0: Int): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        TODO("Not yet implemented")
+    }
+}
